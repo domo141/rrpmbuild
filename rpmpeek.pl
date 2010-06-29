57f72edb93fbc0a12bb5b5e4356cdb2692c12e3e
Tomi Ollila <tomi.ollila@iki.fi>
2010-06-29 15:27:19 +0200

    madde_version sync (0.6.91). first test version of rrpmbuild.pl and helper tool rpmpeek.pl md5sum-checker can distfiles from multiple directories using tarlisted 3.0 (contains archive size fix) some minor tunes

---
+++ rpmpeek.pl
@@ -0,0 +1,262 @@
+#!/usr/bin/perl
+
+# http://www.rpm.org/max-rpm/s1-rpm-file-format-rpm-file-format.html
+# http://docs.fedoraproject.org/drafts/rpm-guide-en/ch-package-structure.html
+
+use strict;
+use warnings;
+
+$" = ', ';
+
+open I, '<', $ARGV[0] or die;
+
+my ($data, $tlen, $toff) = ('', 0, 0);
+sub readdata($)
+{
+    my $len = read I, $data, $_[0];
+    die "$len != $_[0]\n" unless $len == $_[0];
+    $toff = $tlen;
+    $tlen += $len;
+}
+
+sub readlead()
+{
+    # see rpmlead.h (and rmplead.c)
+    readdata 96;
+
+    my @lead = unpack 'NCCnnZ66nnc16', $data;
+
+    printf "\nLead: magic: %x, ", $lead[0];
+    print "major: $lead[1], ";
+    print "minor: $lead[2], ";
+    print "type: $lead[3], ";
+    print "archnum: $lead[4]\n";
+    print "name: $lead[5]\n";
+    print "osnum: $lead[6] ";
+    print "signtype: $lead[7]\n";
+    # don't print reserved (and lead is not used anyway...)
+    # print $lead[8], "\n";
+}
+
+readlead;
+
+my %knownhdrs_pri = ( 62 => 'HDRSIG', 63 => 'HDRIMM', 100 => 'HDRI18N',
+		      267 => 'DSA', 268 => 'RSA', 269 => 'SHA1' );
+my %knownhdrs_sig = ( 1000 => 'SIZE', 1002 => 'PGP', 1004 => 'MD5',
+		      1006 => 'PGP', 1007 => 'PLSIZE' );
+my %knownhdrs_hdr = (
+    1000 => 'NAME', 1001 => 'VERSION', 1002 => 'RELEASE',
+    1003 => 'EPOCH', 1004 => 'SUMMARY', 1005 => 'DESCRIPTION',
+    1006 => 'BUILDTIME', 1007 => 'BUILDHOST', 1008 => 'INSTALLTIME',
+    1009 => 'SIZE', 1010 => 'DISTRIBUTION', 1011 => 'VENDOR',
+    1012 => 'GIF', 1013 => 'XPM', 1014 => 'LICENSE',
+    1015 => 'PACKAGER', 1016 => 'GROUP', 1017 => 'CHANGELOG',
+    1018 => 'SOURCE', 1019 => 'PATCH', 1020 => 'URL',
+    1021 => 'OS', 1022 => 'ARCH', 1023 => 'PREIN',
+    1024 => 'POSTIN', 1025 => 'PREUN', 1026 => 'POSTUN',
+    1027 => 'OLDFILENAMES', 1028 => 'FILESIZES', 1029 => 'FILESTATES',
+    1030 => 'FILEMODES', 1031 => 'FILEUIDS', 1032 => 'FILEGIDS',
+    1033 => 'FILERDEVS', 1034 => 'FILEMTIMES', 1035 => 'FILEDIGESTS',
+    1036 => 'FILELINKTOS', 1037 => 'FILEFLAGS', 1038 => 'ROOT',
+    1039 => 'FILEUSERNAME', 1040 => 'FILEGROUPNAME', 1041 => 'EXCLUDE',
+    1042 => 'EXCLUSIVE', 1043 => 'ICON', 1044 => 'SOURCERPM',
+    1045 => 'FILEVERIFYFLAGS', 1046 => 'ARCHIVESIZE', 1047 => 'PROVIDENAME',
+    1048 => 'REQUIREFLAGS', 1049 => 'REQUIRENAME', 1050 => 'REQUIREVERSION',
+    1051 => 'NOSOURCE', 1052 => 'NOPATCH', 1053 => 'CONFLICTFLAGS',
+    1054 => 'CONFLICTNAME', 1055 => 'CONFLICTVERSION', 1056 => 'DEFAULTPREFIX',
+    1057 => 'BUILDROOT', 1058 => 'INSTALLPREFIX', 1059 => 'EXCLUDEARCH',
+    1060 => 'EXCLUDEOS', 1061 => 'EXCLUSIVEARCH', 1062 => 'EXCLUSIVEOS',
+    1063 => 'AUTOREQPROV', 1064 => 'RPMVERSION', 1065 => 'TRIGGERSCRIPTS',
+    1066 => 'TRIGGERNAME', 1067 => 'TRIGGERVERSION', 1068 => 'TRIGGERFLAGS',
+    1069 => 'TRIGGERINDEX', 1079 => 'VERIFYSCRIPT', 1080 => 'CHANGELOGTIME',
+    1081 => 'CHANGELOGNAME', 1082 => 'CHANGELOGTEXT', 1083 => 'BROKENMD5',
+    1084 => 'PREREQ', 1085 => 'PREINPROG', 1086 => 'POSTINPROG',
+    1087 => 'PREUNPROG', 1088 => 'POSTUNPROG', 1089 => 'BUILDARCHS',
+    1090 => 'OBSOLETENAME', 1091 => 'VERIFYSCRIPTPROG',
+    1092 => 'TRIGGERSCRIPTPROG', 1093 => 'DOCDIR', 1094 => 'COOKIE',
+    1095 => 'FILEDEVICES', 1096 => 'FILEINODES', 1097 => 'FILELANGS',
+    1098 => 'PREFIXES', 1099 => 'INSTPREFIXES', 1100 => 'TRIGGERIN',
+    1101 => 'TRIGGERUN', 1102 => 'TRIGGERPOSTUN', 1103 => 'AUTOREQ',
+    1104 => 'AUTOPROV', 1105 => 'CAPABILITY', 1106 => 'SOURCEPACKAGE',
+    1107 => 'OLDORIGFILENAMES', 1108 => 'BUILDPREREQ', 1109 => 'BUILDREQUIRES',
+    1110 => 'BUILDCONFLICTS', 1111 => 'BUILDMACROS', 1112 => 'PROVIDEFLAGS',
+    1113 => 'PROVIDEVERSION', 1114 => 'OBSOLETEFLAGS',
+    1115 => 'OBSOLETEVERSION', 1116 => 'DIRINDEXES', 1117 => 'BASENAMES',
+    1118 => 'DIRNAMES', 1119 => 'ORIGDIRINDEXES', 1120 => 'ORIGBASENAMES',
+    1121 => 'ORIGDIRNAMES', 1122 => 'OPTFLAGS', 1123 => 'DISTURL',
+    1124 => 'PAYLOADFORMAT', 1125 => 'PAYLOADCOMPRESSOR',
+    1126 => 'PAYLOADFLAGS', 1127 => 'INSTALLCOLOR', 1128 => 'INSTALLTID',
+    1129 => 'REMOVETID', 1130 => 'SHA1RHN', 1131 => 'RHNPLATFORM',
+    1132 => 'PLATFORM', 1133 => 'PATCHESNAME', 1134 => 'PATCHESFLAGS',
+    1135 => 'PATCHESVERSION', 1136 => 'CACHECTIME', 1137 => 'CACHEPKGPATH',
+    1138 => 'CACHEPKGSIZE', 1139 => 'CACHEPKGMTIME', 1140 => 'FILECOLORS',
+    1141 => 'FILECLASS', 1142 => 'CLASSDICT', 1143 => 'FILEDEPENDSX',
+    1144 => 'FILEDEPENDSN', 1145 => 'DEPENDSDICT', 1146 => 'SOURCEPKGID',
+    1147 => 'FILECONTEXTS', 1148 => 'FSCONTEXTS', 1149 => 'RECONTEXTS',
+    1150 => 'POLICIES', 1151 => 'PRETRANS', 1152 => 'POSTTRANS',
+    1153 => 'PRETRANSPROG', 1154 => 'POSTTRANSPROG', 1155 => 'DISTTAG',
+    1156 => 'SUGGESTSNAME', 1157 => 'SUGGESTSVERSION',
+    1158 => 'SUGGESTSFLAGS', 1159 => 'ENHANCESNAME',
+    1160 => 'ENHANCESVERSION', 1161 => 'ENHANCESFLAGS',
+    1162 => 'PRIORITY', 1163 => 'CVSID', 1164 => 'BLINKPKGID',
+    1165 => 'BLINKHDRID', 1166 => 'BLINKNEVRA', 1167 => 'FLINKPKGID',
+    1168 => 'FLINKHDRID', 1169 => 'FLINKNEVRA', 1170 => 'PACKAGEORIGIN',
+    1171 => 'TRIGGERPREIN', 1172 => 'BUILDSUGGESTS',
+    1173 => 'BUILDENHANCES', 1174 => 'SCRIPTSTATES',
+    1175 => 'SCRIPTMETRICS', 1176 => 'BUILDCPUCLOCK',
+    1177 => 'FILEDIGESTALGOS', 1178 => 'VARIANTS',
+    1179 => 'XMAJOR', 1180 => 'XMINOR', 1181 => 'REPOTAG',
+    1182 => 'KEYWORDS', 1183 => 'BUILDPLATFORMS', 1184 => 'PACKAGECOLOR',
+    1185 => 'PACKAGEPREFCOLOR', 1186 => 'XATTRSDICT',
+    1187 => 'FILEXATTRSX', 1188 => 'DEPATTRSDICT',
+    1189 => 'CONFLICTATTRSX', 1190 => 'OBSOLETEATTRSX',
+    1191 => 'PROVIDEATTRSX', 1192 => 'REQUIREATTRSX',
+    1193 => 'BUILDPROVIDES', 1194 => 'BUILDOBSOLETES',
+    1195 => 'DBINSTANCE', 1196 => 'NVRA', 5000 => 'FILENAMES',
+    5001 => 'FILEPROVIDE', 5002 => 'FILEREQUIRE', 5003 => 'FSNAMES',
+    5004 => 'FSSIZES', 5005 => 'TRIGGERCONDS', 5006 => 'TRIGGERTYPE',
+    5007 => 'ORIGFILENAMES', 5008 => 'LONGFILESIZES', 5009 => 'LONGSIZE',
+    5010 => 'FILECAPS', 5011 => 'FILEDIGESTALGO', 5012 => 'BUGURL',
+    5013 => 'EVR', 5014 => 'NVR', 5015 => 'NEVR', 5016 => 'NEVRA',
+    5017 => 'HEADERCOLOR', 5018 => 'VERBOSE', 5019 => 'EPOCHNUM'
+);
+
+sub tagstr($$)
+{
+#    return $_[0];
+    if ($_[0] < 1000) {
+	return $knownhdrs_pri{$_[0]} || 'prihdr';
+    }
+    if ($_[1]) {
+	return $knownhdrs_sig{$_[0]} || 'sighdr';
+    }
+    else { return $knownhdrs_hdr{$_[0]} || 'rpmhdr'; }
+}
+
+sub dt0_null {
+    my ($data, $count) = @_;
+    return "null";
+}
+sub dt1_char {
+    my ($data, $count) = @_;
+    my @out = unpack "c$count", $data;
+    return "char: @out";
+}
+sub dt2_int8 {
+    my ($data, $count) = @_;
+    my @out = unpack "C$count", $data;
+    return "int8: @out";
+}
+sub dt3_int16 {
+    my ($data, $count) = @_;
+    my @out = unpack "n$count", $data;
+    return "int16: @out";
+}
+sub dt4_int32 {
+    my ($data, $count) = @_;
+    my @out = unpack "N$count", $data;
+    return "int32: @out";
+}
+sub dt5_int64 {
+    my ($data, $count) = @_;
+    return "int64 xxx";
+}
+sub dt6_string {
+    my ($data, $count) = @_;
+    my @out = split "\0", $data, $count + 1;
+    pop @out if @out > $count;
+    return "strings:\n" . join "\n----\n", @out if ($count > 1);
+    return "string: @out";
+}
+sub dt7_bin {
+    my ($data, $count) = @_;
+    my @out = unpack "C$count", $data;
+    return sprintf 'bin: ' . '%02x' x $count, @out;
+}
+sub dt8_strarr {
+    my ($data, $count) = @_;
+    my @out = split "\0", $data, $count + 1;
+    pop @out if @out > $count;
+    return "strarr: @out";
+}
+sub dt9_string_i18n {
+    my ($data, $count) = @_;
+    my @out = split "\0", $data, $count + 1;
+    pop @out if @out > $count;
+    return "strings_i18n:\n" . join "\n----\n", @out if ($count > 1);
+    return "string_i18n: @out";
+}
+
+my @dtcalls = (
+    \&dt0_null, \&dt1_char, \&dt2_int8, \&dt3_int16, \&dt4_int32, \&dt5_int64,
+    \&dt6_string, \&dt7_bin, \&dt8_strarr, \&dt9_string_i18n
+);
+
+my ($plfmt, $plcomp) = ('cpio', 'gzip');
+sub chkdatahdr($$)
+{
+    if ($_[0] eq '1124') { # 'PAYLOADFORMAT'
+	$_[1] =~ /(\S+)\s*$/; $plfmt = $1;
+    }
+    elsif ($_[0] eq '1125') { # 'PAYLOADCOMPRESSOR'
+	$_[1] =~ /(\S+)\s*$/; $plcomp = $1;
+    }
+}
+
+sub readheader($)
+{
+    readdata 16;
+
+    my ($magic, $res, $entries, $size);
+    ($magic, $res, $entries, $size) = unpack('NNNN', $data);
+
+    die "'$magic': unknown magic\n" unless $magic == 0x8eade801;
+
+    printf "\nHeader (at %d): magic %x res %d entries %d (%d bytes) size %d\n",
+	$toff, $magic, $res, $entries, ($entries + 1) * 16, $size;
+
+    my @headers;
+    for(1..$entries) {
+	readdata 16;
+	my @list = unpack('NNNN', $data);
+	push @headers, \@list;
+    }
+    readdata $size;
+    foreach (@headers) {
+	my ($tag, $type, $offset, $count) = @$_;
+	printf "%4d(%s) type %d offset %d(%d) count %d\n", $tag,
+	    tagstr($tag, $_[0]), $type, $offset, $toff + $offset, $count;
+	if ($type < @dtcalls) {
+	    my $d = substr $data, $offset;
+	    my $data = $dtcalls[$type]($d, $count);
+	    print '  ', $data, "\n";
+	    chkdatahdr $tag, $data unless $_[0];
+	}
+    }
+}
+
+# 8-align...
+sub readpad () {
+    my $pad = $tlen & 7;
+    readdata 8 - $pad if $pad;
+}
+
+readheader 1;
+readpad;
+readheader 0;
+
+my %plfmtcmds = ( cpio => 'cpio -t --quiet' );
+my %plcompcmds = ( gzip => 'gzip -dc',
+		   bzip2 => 'bzip2 -dc' );
+
+my $plfmtcmd = $plfmtcmds{$plfmt};
+die "'$plfmt': not known playload format\n" unless defined $plfmtcmd;
+my $plcompcmd = $plcompcmds{$plcomp};
+die "'$plcomp': not known playload compressor\n" unless defined $plcompcmd;
+
+print "\nArchive contents (at $tlen):\n";
+
+seek I, $tlen, 0; # to reset buffered data.
+my $fd = fileno I;
+open STDIN, '<&=', $fd;
+system "$plcompcmd | $plfmtcmd";
---
+++ rrpmbuild.pl
@@ -0,0 +1,567 @@
+#!/usr/bin/perl
+# -*- cperl -*-
+
+# This file is part of MADDE
+#
+# Copyright (C) 2010 Nokia Corporation and/or its subsidiary(-ies).
+#
+# Contact: Riku Voipio <riku.voipio@nokia.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public License
+# version 2.1 as published by the Free Software Foundation.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA
+
+use strict;
+use warnings;
+
+use Fcntl ':mode';
+use File::Find;
+
+use Digest::MD5;
+
+# SO far, this is proof-of-concept implementation (read: terrible hack).
+# Even some mentioned features do not work... and tested only on linux
+# with perl 5.10.x // 2010-06-10 too
+
+# Welcome to the source of 'restricted' rpmbuild. The 'restrictions' are:
+#  we do not recurse! (some is easy to add (and even full recursion)
+#  no fancy variables
+#  a se if %macros just not supported
+#  only binary rpms (so far)
+#  only -bb now, more -b -optios later. some --:s too...
+
+
+# bottom line: the spec files that rrpmbuild can build, are also buildable
+# with standard rpm, but not wise versa.
+
+# from rpm-4.8.0/rpmrc.in
+
+my %arch_canon = ( i686 => 1, i586 => 1, i486 => 1, i386 => 1, x86_64 => 1,
+		   armv3l => 12, armv4b => 12, armv4l => 12, armv5tel => 12,
+		   armv5tejl => 12, armv6l => 12, armv7l => 12 );
+
+my %os_canon = ( linux => 1 );
+
+# packages array defines order for packages + other hashes.
+my @pkgnames = ('');
+my %packages = ('', [ [ ], { } ] );
+
+my (@prep, @build, @install, @clean);
+my (%description, %files);
+my (%pre, %post, %preun, %postun);
+my @changelog;
+
+my %macros;
+sub init_macros()
+{
+    my ($prefix, $exec_prefix, $lib) = ( '/usr', '/usr', 'lib' );
+
+    %macros = ( _prefix => $prefix,
+		_exec_prefix => $exec_prefix,
+		_exec_prefix => $exec_prefix,
+		_bindir => $exec_prefix . '/bin',
+		_sbindir => $exec_prefix . '/sbin',
+		_libexecdir => $exec_prefix . '/libexec',
+		_datadir => $prefix . '/share',
+		_sysconfdir => $prefix . '/etc',
+		_sharedstatedir => $prefix . '/com',
+		_localstatedir => $prefix . '/var',
+		_lib => $lib,
+		_libdir => $exec_prefix . '/' . $lib,
+		_includedir => $prefix . '/include',
+		_oldincludedir => '/usr/include',
+		_infodir => $prefix . '/info',
+		_mandir => $prefix . '/man',
+
+		setup => 'echo no %prep' );
+}
+
+my $instroot = 'rrpmbuild/instroot';
+my $instrlen = length $instroot;
+
+$ENV{'RPM_BUILD_ROOT'} = $instroot;
+$ENV{'RPM_OPT_FLAGS'} = '-O2';
+#$ENV{''} = '';
+#$ENV{''} = '';
+
+sub rest_macros()
+{
+    sub NL() { "\n"; }
+    $macros{'buildroot'} = $ENV{'RPM_BUILD_ROOT'};
+    $macros{'makeinstall'} = eval_macros ( 'make install \\' . NL .
+		'  prefix=%{buildroot}/%{_prefix} \\' . NL .
+		'  exec_prefix=%{buildroot}/%{_exec_prefix} \\' . NL .
+		'  bindir=%{buildroot}/%{_bindir} \\' . NL .
+		'  sbindir=%{buildroot}/%{_sbindir} \\' . NL .
+		'  sysconfdir=%{buildroot}/%{_sysconfdir} \\' . NL .
+		'  datadir=%{buildroot}/%{_datadir} \\' . NL .
+		'  includedir=%{buildroot}/%{_includedir} \\' . NL .
+		'  libdir=%{buildroot}/%{_libdir} \\' . NL .
+		'  libexecdir=%{buildroot}/%{_libexecdir} \\' . NL .
+		'  localstatedir=%{buildroot}/%{_localstatedir} \\' . NL .
+		'  sharedstatedir=%{buildroot}/%{_sharedstatedir} \\' . NL .
+		'  mandir=%{buildroot}/%{_mandir} \\' . NL .
+		'  infodir=%{buildroot}/%{_infodir}'
+	      ) unless defined $macros{'makeinstall'};
+}
+
+my %stanzas = ( package => 1, description => 1, changelog => 1,
+		prep => 1, build => 1, install => 1, clean => 1,
+		files => 1, pre => 1, post => 1, preun => 1, postun => 1 );
+sub readspec()
+{
+    sub eval_macros($)
+    {
+	my ($_, $rest) = split '#', $_[0], 2;
+
+	sub _eval_it() {
+	    return '%' if $1 eq '%';
+	    return $macros{$1} if defined $macros{$1};
+	    die "'$1': undefined macro\n";
+	}
+
+	#    s/%%/\001/g;
+	# dont be too picky if var is in format %{foo or $foo} ;) (i.e fix ltr)
+	s/%\{?(%|[\w\?\!]+)\}?/_eval_it/ge;
+	#    s/\001/%/g;
+	return $_ . '#' . $rest if (defined $rest);
+	return $_;
+    }
+
+    sub readpackage($)
+    {
+	my ($arref, $hashref) = ($_[0]->[0], $_[0]->[1]);
+
+	while (<I>) {
+	    s/#.*//;
+	    next if /^\s*$/;
+	    if (/^\s*%define\s+(\S+)\s+(.*?)\s*$/) {
+		$macros{$1} = eval_macros $2;
+		next;
+	    }
+	    last if /^\s*%/;
+	    if (/^\s*(\S+?)\s*:\s*(.*?)\s+$/) {
+		my $key = lc $1;
+		my $val = $hashref->{$key};
+		if (defined $val) {
+		    $val = $val . ', ' . eval_macros $2;
+		}
+		else {
+		    push @$arref, $key;
+		    $val = eval_macros $2;
+		}
+		$hashref->{$key} = $val;
+		# Add format checks, too...
+		$macros{$key} = $val
+		  if $key eq 'name' || $key eq 'version' || $key eq 'release';
+		next;
+	    }
+	    chomp;
+	    die "'$_': unknown header format\n";
+	}
+    }
+
+    sub readlines($)
+    {
+	while (<I>) {
+	    return if /^\s*%(\S+)/ && defined $stanzas{$1};
+	    push @{$_[0]}, eval_macros $_;
+	}
+    }
+
+    sub readfiles($)
+    {
+	# doing stuff to catch more errors early, i.e. not after build
+	# if these lines were just listed and all scanning done after
+
+	$macros{$_} = "\001$_\001" foreach (qw/defattr attr doc dir config/);
+	delete $macros{'docdir'}; delete $macros{'verify'};
+
+	# xxx later may check format of defattr and attr...
+	readlines $_[0];
+
+	delete $macros{$_} foreach (qw/defattr attr doc dir config/);
+    }
+
+    sub readignore($)
+    {
+	while (<I>) {
+	    return if /^\s*%(\S+)/ && defined $stanzas{$1};
+	}
+    }
+
+    readpackage ($packages{''});
+    while (1) {
+	chomp, die "'$_': unsupported stanza format.\n"
+	  unless /^\s*%(\w+)\s*(\S*?)\s*$/;
+
+	if ($1 eq 'package') {
+	    push @pkgnames, $2;
+	    readpackage ($packages{$2} = [ [ ], { } ]);
+	}
+	elsif ($1 eq 'description') { readlines ($description{$2} = [ ]); }
+
+	elsif ($1 eq 'prep') {    readignore \@prep; }
+	elsif ($1 eq 'build') {   readlines \@build; }
+	elsif ($1 eq 'install') { readlines \@install; }
+	elsif ($1 eq 'clean') {   readlines \@clean; }
+
+	elsif ($1 eq 'files') { readfiles ($files{$2} = [ ]); }
+	elsif ($1 eq 'pre') { readlines ($pre{$2} = [ ]); }
+	elsif ($1 eq 'post') { readlines ($post{$2} = [ ]); }
+	elsif ($1 eq 'preun') { readlines ($preun{$2} = [ ]); }
+	elsif ($1 eq 'postun') { readlines ($postun{$2} = [ ]); }
+
+	elsif ($1 eq 'changelog') { readlines \@changelog; }
+
+	else { chomp; die "'$1': unsupported stanza macro.\n"; }
+	last if eof I;
+    };
+}
+
+die "Usage: $0 -bb <specfile>\n" unless @ARGV == 2 && $ARGV[0] eq '-bb';
+die "$ARGV[1]: not a file\n" unless -f $ARGV[1];
+
+init_macros;
+open I, '<', $ARGV[1] or die;
+readspec;
+close I;
+foreach (qw/name version release/) {
+    die "Package $_ not known\n" unless (defined $macros{$_});
+}
+rest_macros;
+
+# XXX check what must be in "sub" packages
+foreach (qw/license summary group/) {
+    die "Package $_ not known\n" unless (defined $packages{''}->[1]->{$_});
+}
+
+
+# check that we have description and files for all packages
+# description and/or files sections that do not have packages
+# are just ignored (should we care ?=
+
+foreach (@pkgnames) {
+    die "No 'description' section for package '$_'\n"
+      unless defined $description{$_};
+    die "No 'files' section for package '$_'\n"
+      unless defined $files{$_};
+}
+
+sub execute_stage($$)
+{
+    print "Executing: %$_[0]\n";
+    system('/bin/sh', '-exc', $_[1]);
+    if ($?) {
+	my $ev = $? >> 8;
+	die "$_[0] exited with nonzero exit code ($ev)\n";
+    }
+}
+
+#skip prep ## and fix...
+#execute_stage 'clean', join '', @clean;
+
+execute_stage 'build', join '', @build;
+execute_stage 'install', join '', @install;
+
+my ($ino, $cpio_dsize);
+sub open_cpio_file($)
+{
+    open STDOUT, '>', $_[0] or die "Open $_[0] failed: $!\n";
+    $ino = 1;
+    $cpio_dsize = 0;
+}
+
+# knows files & directories.
+sub file_to_cpio($$$)
+{
+    my ($name, $mode, $file) = @_;
+    my ($size, $mtime, $nlink);
+
+    if (defined $file) {
+	my @sb = stat $file or die "stat '$file': $!\n";
+	$mtime = $sb[9];
+	if (S_ISDIR($sb[2])) {
+	    $size = 0; $mode += 0040000; $nlink = 2;
+	}
+	else { $size = $sb[7]; $mode += 0100000; $nlink = 1; }
+    }
+    else { $mtime = 0; $size = 0; $mode = 0; $nlink = 1; }
+
+    my $namesize = length($name) + 1;
+    my $hdrbytes = 110 + $namesize;
+    $hdrbytes += 4 - ($hdrbytes & 0x03) if ($hdrbytes & 0x03);
+    # Type: New ASCII without crc (070701). See librachive/cpio.5
+    syswrite STDOUT, sprintf
+      ("070701%08x%08x%08x%08x%08x%08x%08x%08x%08x%08x%08x%08x00000000%s\000\000\000\000", $ino, $mode,0,0, $nlink, $mtime, $size, 0,0,0,0, $namesize, $name),
+	$hdrbytes;
+
+    $cpio_dsize += $size, system ('/bin/cat', $file) if $size;
+    if ($size & 0x03) {
+	syswrite STDOUT, "\000\000\000", 4 - ($size & 0x03);
+    }
+    return ($mode, $size, $mtime);# if wantarray;
+}
+
+sub close_cpio_file()
+{
+    $ino = 0;
+    file_to_cpio('TRAILER!!!', 0, undef);
+    # not making size multiple of 512 (as doesn't rpm do either).
+    open STDOUT, ">&STDERR" or die;
+}
+
+
+# make cpio, fill arrays
+foreach (@pkgnames)
+{
+    my ($fmode, $dmode, $uname, $gname, $isdoc, $isconfig);
+    my ($wdir, $pkgname, $spkg);
+
+    sub getmd5sum($)
+    {
+	my $ctx = Digest::MD5->new;
+	#XXX warn $_[0];
+	open J, '<', $_[0] or die $!;
+	$ctx->addfile(*J);
+	close J;
+	return $ctx->hexdigest;
+    }
+
+    my (@files, @dirindexes, @dirs, %dirs, @modes, @sizes, @mtimes);
+    my (@unames, @gnames, @md5sums);
+    sub add2lists($$$$$)
+    {
+	$_[0] =~ m%(.*/)(.+)% or die "'$_[0]': invalid path\n";
+	my ($dir, $base) = ($1, $2);
+	my $di = $dirs{$dir};
+	unless (defined $di) {
+	    $di = $dirs{$dir} = scalar @dirs;
+	    push @dirs, $dir;
+	}
+	push @files, $base;
+	push @dirindexes, $di;
+
+	push @modes, $_[1];
+	push @sizes, $_[2];
+	push @mtimes, $_[3];
+	push @unames, $uname;
+	push @gnames, $gname;
+	if (defined $_[4]) {
+	    push @md5sums, getmd5sum $_[4];
+	}
+	else { push @md5sums, ''; }
+    }
+
+    sub addocfile($) {
+	#if (/\*/)...
+	# XXX pkgname has ${release}...
+	warn "Adding doc file $_[0]\n";
+	my $fname = "usr/share/doc/$pkgname/" . $_[0];
+	my ($mode, $size, $mtime) = file_to_cpio($fname, $fmode, $_[0]);
+	add2lists($fname, $mode, $size, $mtime, $_[0]);
+    }
+
+    my @_flist;
+    sub _addfile($$);
+    sub _addfile($$)
+    {
+	if (-d "$instroot/$_[0]") {
+	    warn "Adding directory $_[0]\n";
+	    my ($mode,$size,$mtime) = file_to_cpio($_[0], $dmode, "$instroot/$_[0]");
+	    add2lists($_[0], $mode, $size, $mtime, undef);
+
+	    return if $_[1];
+	    sub _f() { push @_flist, (substr $_, $instrlen + 1); }
+	    @_flist = ();
+	    find({wanted =>\&_f, no_chdir => 1}, "$instroot/$_[0]");
+	    shift @_flist;
+	    _addfile($_, 1) foreach ( sort @_flist );
+	    return;
+	}
+	warn "Adding file $_[0]\n";
+	my ($mode,$size,$mtime) = file_to_cpio($_[0], $fmode, "$instroot/$_[0]");
+	add2lists($_[0], $mode, $size, $mtime, "$instroot/$_[0]");
+    }
+    sub addfile($$)
+    {
+	my $_ = $_[0];
+	if (/\*/) {
+	    foreach ( glob "$instroot/$_" ) {
+		_addfile substr($_, $instrlen + 1), $_[1];
+	    }
+	    return;
+	}
+	_addfile $_, $_[1];
+    }
+
+    sub createsigheader($$)
+    {
+	# all hardcoded (use _append later, when proof-of-concept ready)
+	my @hdr;
+
+	push @hdr, pack("CCCCNNN", 0x8e, 0xad, 0xe8, 0x01, 0, 2, 20);
+	push @hdr, pack("NNNN", 1000, 4, 0, 1); # SIZE
+	push @hdr, pack("NNNN", 1004, 7, 4, 16); # MD5
+
+	push @hdr, pack("N", $_[0]); # add SIZE;
+	push @hdr, $_[1]; # add digest
+	return join('', @hdr) . "\0" x 4; # with align
+    }
+
+    my (@cdh_index, @cdh_data, $cdh_offset, $cdh_extras);
+    sub createdataheader($$) # spkg, cpiofile
+    {
+	@cdh_index = (); @cdh_data = (); $cdh_offset = 0; $cdh_extras = 0;
+	sub _append($$$$)
+	{
+	    my ($tag, $type, $count, $data) = @_;
+
+	    if ($type == 3) { # int16, align by 2
+		$cdh_extras++, $cdh_offset++, push @cdh_data, "\000"
+		  if ($cdh_offset & 1);
+	    }
+	    elsif ($type == 4) { # int32, align by 4
+		if ($cdh_offset & 3) {
+		    my $pad = 4 - ($cdh_offset & 3);
+		    $cdh_extras++;
+		    $cdh_offset += $pad, push @cdh_data, "\000" x $pad;
+		}
+	    }
+	    # elif $type == 5) #int64 align by 8...
+
+	    push @cdh_index, pack("NNNN", $tag, $type, $cdh_offset, $count);
+	    push @cdh_data, $_[3];
+	    $cdh_offset += length $_[3];
+	    warn 'Pushing data "', $_[3], '"', "\n" if $type == 6;
+	}
+
+	_append(100, 6, 1, "C\000"); # hdri18n, atm
+
+	_append(1000, 6, 1, "$macros{name}\000"); # name
+	_append(1001, 6, 1, "$macros{version}\000"); # version
+	_append(1002, 6, 1, "$macros{release}\000"); # release
+	_append(1004, 9, 1, "$packages{$_[0]}->[1]->{summary}\000"); # summary, atm
+	my $description = join '', @{$description{$_[0]}};
+	$description =~ s/\s+$//;
+	_append(1005, 6, 1, "$description\000"); # descrip, atm
+	_append(1009, 4, 1, pack("N", $cpio_dsize) ); # size
+	_append(1014, 6, 1, "$packages{''}->[1]->{license}\000"); # license, atm
+	_append(1016, 6, 1, "$packages{$_[0]}->[1]->{group}\000"); # group, atm
+	_append(1021, 6, 1, "Linux\000"); # os
+	_append(1022, 6, 1, "arm\000"); # arch
+	_append(1046, 4, 1, pack("N", -s $_[1]) ); # archivesize
+	_append(1047, 6, 1, "$macros{name}\000"); # providename
+	_append(1124, 6, 1, "cpio\000"); # payloadfmt
+	_append(1125, 6, 1, "gzip\000"); # payloadcomp
+
+	my $count;
+	$count = scalar @files;
+	_append(1117, 8, $count, join("\000", @files) . "\000");
+	$count = scalar @dirs;
+	_append(1118, 8, $count, join("\000", @dirs) . "\000");
+	$count = scalar @dirindexes;
+	_append(1116, 4, $count, pack "N" . $count, @dirindexes);
+	$count = scalar @modes;
+	_append(1030, 3, $count, pack "n" . $count, @modes);
+	$count = scalar @sizes;
+	_append(1028, 4, $count, pack "N" . $count, @sizes);
+	$count = scalar @mtimes;
+	_append(1034, 4, $count, pack "N" . $count, @mtimes);
+	$count = scalar @md5sums;
+	_append(1035, 8, $count, join("\000", @md5sums) . "\000");
+	$count = scalar @unames;
+	_append(1039, 8, $count, join("\000", @unames) . "\000");
+	$count = scalar @gnames;
+	_append(1040, 8, $count, join("\000", @gnames) . "\000");
+
+	# align 8 for making shure...
+	if ($count & 7) {
+	    my $pad = 8 - ($count & 7);
+	    $cdh_extras++;
+	    $cdh_offset += $pad, push @cdh_data, "\000" x $pad;
+	}
+
+	my $header = join '', @cdh_data;
+	my $hdrhdr = pack "CCCCNNN", 0x8e, 0xad, 0xe8, 0x01, 0,
+	  scalar @cdh_data - $cdh_extras, length $header;
+
+	return $hdrhdr . join('', @cdh_index) . $header;
+    }
+
+    $spkg = $_;
+    if (length $_) {
+	$pkgname = "$macros{name}-$spkg-$macros{version}-$macros{release}";
+    }
+    else {
+	$pkgname = "$macros{name}-$macros{version}-$macros{release}";
+    }
+    $wdir = 'rrpmbuild/' . $pkgname;
+
+    system ('/bin/rm', '-rf', $wdir);
+    system ('/bin/mkdir', '-p', $wdir);
+
+    my ($deffmode, $defdmode, $defuname, $defgname) = qw/0644 0755 root root/;
+
+    my $cpiofile = $wdir . '/cpio';
+    open_cpio_file $cpiofile;
+
+    LINE: foreach (@{$files{$spkg}}) {
+	($fmode, $dmode, $uname, $gname) = ($deffmode,$defdmode,$defuname,$defgname);
+	my ($isdir, $isconfig, $isdoc) = (0, 0, 0);
+	while (1) {
+	    if (s/\001(def)?attr\001\((.+?)\)//) {
+		my @attrs = split /\s*,\s*/, $2;
+
+		$fmode = $attrs[0] if (defined $attrs[0] && $attrs[0] ne '-');
+		$uname = $attrs[1] if (defined $attrs[1] && $attrs[1] ne '-');
+		$gname = $attrs[2] if (defined $attrs[2] && $attrs[2] ne '-');
+		$dmode = $attrs[3] if (defined $attrs[3] && $attrs[3] ne '-');
+		# XXX should check that are numeric and in right range.
+		$fmode = oct $fmode; $dmode = oct $dmode;
+		($deffmode,$defdmode,$defuname,$defgname)
+		  = ($fmode, $dmode, $uname, $gname) if defined $1;
+		next;
+	    }
+	    $isdir = 1, next if s/\001dir\001//;
+	    $isconfig = 1, next if s/\001config\001//;
+	    # last, as slurps end of line (won't do better! ambiquous if.)
+	    if (s/\001doc\001//) {
+		$isdoc = 1;
+		foreach (split /\s+/) {
+		    addocfile $_ if length $_;
+		}
+		next LINE;
+	    }
+	    last;
+	}
+	addfile $1, $isdir if /^\s*(\S+)\s+$/; # XXX no whitespace in filenames
+    }
+    close_cpio_file;
+
+    my $dhdr = createdataheader $spkg, $cpiofile;
+    system 'gzip', $cpiofile;
+    my $ctx = Digest::MD5->new();
+    $ctx->add($dhdr);
+    open J, "$wdir/cpio.gz" or die $!;
+    $ctx->addfile(*J);
+    close J;
+    my $shdr = createsigheader length($dhdr) + -s "$cpiofile.gz", $ctx->digest;
+
+    open STDOUT, '>', "$wdir.rpm" or die $!;
+    my $leadname = substr "$pkgname.rpm", 0, 65;
+    print pack 'NCCnnZ66nnZ16', 0xedabeedb, 3, 0, 0, 12, $leadname, 1, 5, "\0";
+    print $shdr, $dhdr;
+    system('/bin/cat' ,"$cpiofile.gz");
+    open STDOUT, ">&STDERR" or die;
+}
