6e7fb688f091d4faef7fffeeeb1e6d2022dde934
Tomi Ollila <tomi.ollila@iki.fi>
2013-03-29 16:10:25 +0200

    rpmpeek.pl: acced xz extraction support

--- rpmpeek.pl
+++ rpmpeek.pl
@@ -282,7 +282,8 @@ if (-x "$_/xcpio") {
 
 my %plfmtcmds = ( cpio => $cpiocmd );
 my %plcompcmds = ( gzip => 'gzip -dc',
-		   bzip2 => 'bzip2 -dc' );
+		   bzip2 => 'bzip2 -dc',
+		   xz => 'xz -dc' );
 
 my $plfmtcmd = $plfmtcmds{$plfmt};
 die "'$plfmt': not known playload format\n" unless defined $plfmtcmd;
