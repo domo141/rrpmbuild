ac54239ef0f6218774084abbbd1c1a4246be9aa3
Tomi Ollila <tomi.ollila@iki.fi>
2014-03-04 21:18:57 +0200

    make debian package look more indistinguishable:
    ar in bsd format, tars in gnu format.

--- mkpkgs.sh
+++ mkpkgs.sh
@@ -22,8 +22,10 @@ die () { echo "$@" >&2; exit 1; }
 
 mkvers ()
 {
+	set x `exec git log -1 --pretty=%ct`
+	SECS=$2
 	IFS="${IFS}-"
-	set x `git log --pretty=%ci | sed 's/ .*//' | uniq -c | sed q`
+	set x `exec git log --pretty=%ci | sed 's/ .*//' | uniq -c | sed q`
 	IFS=
 	VERS=$3.$4.$5
 	RELN=$2
@@ -48,7 +50,7 @@ cp rpmpeek.pl rrpmbuild.pl dot-build/usr/bin
 
 SIZE=`du -ks dot-build | tr -dc 0-9`
 
-export TAR_OPTIONS="--format=ustar --owner=root --group=root"
+export TAR_OPTIONS="--format=gnu --owner=root --group=root"
 
 PKG=rrpmbuild
 
@@ -72,8 +74,15 @@ Description: R rpm build tool
 
 ( cd dot-build/DEBIAN; exec tar zcf control.tar.gz ./control )
 ( cd dot-build; exec tar --exclude '[DR][EP][BM]*' -zcf DEBIAN/data.tar.gz . )
-( cd dot-build/DEBIAN; exec ar q ../${PKG}_${VERS}-${RELN}_all.deb \
-	debian-binary control.tar.gz data.tar.gz )
+( cd dot-build/DEBIAN;
+  ln ../../mkpkgs.sh .
+  ARCHIVE_TIME=$SECS; export ARCHIVE_TIME
+  exec > ../${PKG}_${VERS}-${RELN}_all.deb
+  echo '!<arch>'
+  perl -x "$0" addstr2ar debian-binary '2.0
+'
+  perl -x "$0" addfile2ar control.tar.gz
+  perl -x "$0" addfile2ar data.tar.gz )
 
 mkdir dot-build/RPM
 echo > dot-build/RPM/rpm.spec "\
@@ -113,3 +122,53 @@ echo installed
 mv dot-build/RPM/*.rpm dot-build
 set -x
 ls -l dot-build
+exit
+
+#!perl
+#line 129
+# warn 'at mkpkgs.sh line 129';
+
+# (progn (cperl-mode) (set-variable 'cperl-indent-level 8))
+# (progn (shell-script-mode) (set-variable 'sh-basic-offset 8))
+
+use strict;
+use warnings;
+
+sub copyIO($)
+{
+  my $left = $_[0];
+  while ($left > 65536) {
+	my $l = sysread I, $_, 65536;
+	die "Read error: $!\n" unless $l;
+	die "Write error: $!\n" unless syswrite STDOUT, $_;
+	$left -= $l;
+ }
+  while ($left > 0) {
+	my $l = sysread I, $_, $left;
+	die "Read error: $!\n" unless $l;
+	die "Write error: $!\n" unless syswrite STDOUT, $_;
+	$left -= $l;
+ }
+}
+
+if ($ARGV[0] eq 'addfile2ar')
+{
+        open I, '<', $ARGV[1] or die "Cannot read '$ARGV[0]/$_[0]': $!\n";
+        my $size = -s $ARGV[1];
+        my $time = $ENV{ARCHIVE_TIME};
+        syswrite STDOUT, sprintf "%-16s%d  0     0     100644  %-10d\140\012",
+                $ARGV[1], $time, $size;
+        copyIO $size;
+        syswrite STDOUT, "\n", 1 if $size & 1;
+        close I;
+}
+
+if ($ARGV[0] eq 'addstr2ar')
+{
+        my $size = length $ARGV[2];
+        my $time = $ENV{ARCHIVE_TIME};
+        syswrite STDOUT, sprintf "%-16s%d  0     0     100644  %-10d\140\012",
+                $ARGV[1], $time, $size;
+	syswrite STDOUT, $ARGV[2];
+        syswrite STDOUT, "\n", 1 if $size & 1;
+}
